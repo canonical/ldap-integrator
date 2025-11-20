78ae6d64762006e0dfd390d462b9772e67ba273d
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2025-11-20 17:28:52 -0500

    feat: migrate unit tests from legacy `Harness` library to `ops-scenario`
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,12 +1,10 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from typing import Dict, Generator, Tuple
-from unittest.mock import MagicMock, create_autospec
+from unittest.mock import MagicMock
 
 import pytest
-from ops import EventBase
-from ops.testing import Harness
+from ops import testing
 from pytest_mock import MockerFixture
 
 from charm import LdapIntegratorCharm
@@ -14,63 +12,39 @@ from constants import LDAP_INTEGRATION_NAME
 
 
 @pytest.fixture
-def harness() -> Generator[Harness, None, None]:
-    harness = Harness(LdapIntegratorCharm)
-    harness.set_model_name("unit-test")
-    harness.set_leader(True)
-
-    harness.begin_with_initial_hooks()
-    yield harness
-    harness.cleanup()
+def all_satisfied_conditions(mocker: MockerFixture) -> None:
+    mocker.patch("charm.ldap_integration_exists", return_value=True)
 
 
 @pytest.fixture
-def mocked_event() -> MagicMock:
-    return create_autospec(EventBase)
-
-
-@pytest.fixture
-def password_secret(harness: Harness) -> Tuple[str, str]:
-    bind_password = "bind_password"
-    secret = harness.add_user_secret({"password": bind_password})
-    harness.grant_secret(secret, harness.charm.app)
-    return bind_password, secret
-
-
-@pytest.fixture
-def charm_configuration(harness: Harness, password_secret: Tuple[str, str]) -> Dict:
-    secret = harness.add_user_secret({"password": "secret"})
-    harness.grant_secret(secret, harness.charm.app)
+def charm_configuration(password_secret: testing.Secret) -> dict:
     config = {
         "urls": "ldap://ldap.com/path/to/somewhere",
         "ldaps_urls": "ldaps://ldap.com/path/to/somewhere",
         "base_dn": "dc=glauth,dc=com",
         "starttls": True,
         "bind_dn": "cn=user,ou=group,dc=glauth,dc=com",
-        "bind_password": password_secret[1],
+        "bind_password": password_secret.id,
         "auth_method": "simple",
     }
-    harness.update_config(config)
     return config
 
 
 @pytest.fixture
-def ldap_integration(harness: Harness) -> int:
-    return harness.add_relation(LDAP_INTEGRATION_NAME, "ldap")
+def ctx() -> testing.Context[LdapIntegratorCharm]:
+    return testing.Context(LdapIntegratorCharm)
 
 
 @pytest.fixture
-def ldap_integration_data(harness: Harness, ldap_integration: int) -> None:
-    harness.update_relation_data(
-        ldap_integration,
-        "ldap",
-        {
-            "user": "user",
-            "group": "group",
-        },
-    )
+def ldap_integration() -> testing.Relation:
+    return testing.Relation(endpoint=LDAP_INTEGRATION_NAME, remote_app_name="ldap")
 
 
 @pytest.fixture
-def all_satisfied_conditions(mocker: MockerFixture) -> None:
-    mocker.patch("charm.ldap_integration_exists", return_value=True)
+def mock_ldap_provider(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch("charm.LdapProvider.update_relations_app_data")
+
+
+@pytest.fixture
+def password_secret() -> testing.Secret:
+    return testing.Secret(tracked_content={"password": "bind_password"})
